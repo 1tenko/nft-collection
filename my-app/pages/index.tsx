03e03b933f5ec9de6d527ad69bc35e3cc202dff8
tenko <tajm.ogunbiyi@gmail.com>
2022-07-13 22:54:57 +0100

    :construction_worker: Created getProviderOrSigner, presaleMint, publicMint and connectWallet functions

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -23,6 +23,86 @@ const Home: NextPage = () => {
   // reference to the Web3 Modal (used for connecting metamask) which persists as long as the page is open
   const web3ModalRef = useRef();
 
+  // returns a provider or signer object representing the Ethereum RPC with or without the signing capabilities of metamask attached
+  // true if you need signer, default false otherwise
+  const getProviderOrSigner = async (needSigner = false) => {
+    // connect to metamask
+    // since we store 'web3Modal' as a reference, we need o access the 'current' value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // if user is not connected to the Rinkeby network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
+    }
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  // Mint an NFT during the presale
+  const presaleMint = async () => {
+    try {
+      // we need signer here since this is a 'write' transaction
+      const signer = await getProviderOrSigner(true);
+      // create new instance of the Contract with a Signer, which allows update methods
+      const whitelistContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      // call presaleMint from the contract, only whitelisted addresses would be able to mint
+      const tx = await whitelistContract.presaleMint({
+        // value signifies the cost of one crypto dev which is '0.01' eth.
+        // we are parsing '0.01' string to ether using the utils library from ethers.js
+        value: utils.parseEther("0.01"),
+      });
+      setLoading(true);
+      // wait for transaction to get mined
+      await tx.wait();
+      setLoading(false);
+      window.alert("You successfully minted a Crypto Dev!");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // mint an NFT after the presale
+  const publicMint = async () => {
+    try {
+      // we need signer here since this is a 'write' transaction
+      const signer = await getProviderOrSigner(true);
+      // create new instance of the Contract with a Signer, which allows update methods
+      const whitelistContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      // call presaleMint from the contract, only whitelisted addresses would be able to mint
+      const tx = await whitelistContract.presaleMint({
+        // value signifies the cost of one crypto dev which is '0.01' eth.
+        // we are parsing '0.01' string to ether using the utils library from ethers.js
+        value: utils.parseEther("0.01"),
+      });
+      setLoading(true);
+      // wait for transaction to get mined
+      await tx.wait();
+      setLoading(false);
+      window.alert("You successfully minted a Crypto Dev!");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      // get the provider from web3Modal, which in our case is metamask
+      // when used for the first time,  it prompts the user to connect their wallet
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // starts the presale for the NFT collection
+
   return (
     <div>
       <Head>
@@ -39,16 +119,14 @@ const Home: NextPage = () => {
           <div className={styles.description}>
             {tokenIdsMinted}/20 have been minted
           </div>
-          {renderButton()}
+          {/* {renderButton()} */}
         </div>
         <div>
-          <img className={styles.image} src="./cryptodevs/0.svg" />
+          <img className={styles.image} src="./0CDNFT.svg" />
         </div>
       </div>
 
-      <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
-      </footer>
+      <footer className={styles.footer}>Made with &#10084; by Tenko</footer>
     </div>
   );
 };
